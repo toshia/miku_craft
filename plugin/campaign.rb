f55e0fa546af6a34cfe1e252f4d9470eee343f79
Toshiaki Asai <toshi.alternative@gmail.com>
2016-03-05 23:54:44 +0900

    自殺強化月間

--- plugin/campaign.rb
+++ plugin/campaign.rb
@@ -18,21 +18,23 @@ module Plugin::Campaign
     end
   end
 
+  YEAR = Time.now.year
+
   Campaigns = [
     Campaign.new(name: "人権週間キャンペーン",
-                 range: Range.new(Date.new(2015, 12, 4), Date.new(2015, 12, 10), false))
+                 range: Range.new(Date.new(YEAR, 12, 4), Date.new(YEAR, 12, 10), false))
     .on_daily_login{ |name, campaign|
       Plugin.call(:minecraft_tell, name, '人権週間キャンペーン開催中(12/10まで)！一日乗車券をプレゼント！')
       Plugin.call(:minecraft_give_item, name, 'minecraft:paper', 1, 0, "{display:{Name:\"一日乗車券\"}}")
     },
     Campaign.new(name: "mikutter#{Time.now.year - 2009}周年記念キャンペーン(12/25まで)",
-                 range: Range.new(Date.new(2015, 12, 20), Date.new(2015, 12, 25), false))
+                 range: Range.new(Date.new(YEAR, 12, 20), Date.new(YEAR, 12, 25), false))
       .on_daily_login{ |name, campaign|
       Plugin.call(:minecraft_tell, name, "#{campaign.name}！誕生日ケーキをプレゼント！")
       Plugin.call(:minecraft_give_item, name, 'minecraft:cake', 1, 0, '{display:{Name: "誕生日ケーキ",Lore:["mikutterの誕生日記念"]}}')
     },
     Campaign.new(name: "年末年始キャンペーン(1/5まで)",
-                 range: Range.new(Date.new(2015, 12, 25), Date.new(2016, 1, 5), false))
+                 range: Range.new(Date.new(YEAR, 12, 25), Date.new(YEAR+1, 1, 5), false))
       .on_daily_login{ |name, campaign|
       [ -> {
           Plugin.call(:minecraft_tell, name, "#{campaign.name}！門松をプレゼント！")
@@ -51,8 +53,29 @@ module Plugin::Campaign
           Plugin.call(:minecraft_give_item, name, 'minecraft:skull', 1, 3, %<{display:{Name:"獅子舞",Lore:["メリーお正月！"]},SkullOwner:"#{name}",AttributeModifiers:[{AttributeName:"generic.maxHealth",Name:"generic.maxHealth",Amount:2,Operation:0,UUIDMost:77658,UUIDLeast:855423}],ench:[{id:6,lvl:1}],HideFlags:1}>)
         }
       ].sample.()
-    }
-
+    },
+    Campaign.new(name: "自殺強化月間",
+                 range: Range.new(Date.new(YEAR, 3, 1), Date.new(YEAR, 3, 31), false))
+      .on_daily_login{ |name, campaign|
+      today = Time.now.strftime("%Y/%m/%d")
+      [ -> {
+          Plugin.call(:minecraft_tell, name, "#{campaign.name}！蜘蛛の目をプレゼント！")
+          Plugin.call(:minecraft_give_item, name, 'minecraft:spider_eye', 1, 0, %<{display:{Name: "蜘蛛の目",Lore:["自殺強化月間記念に#{name}がもらった", "#{today}"]}}>)
+        },
+        -> {
+          Plugin.call(:minecraft_tell, name, "#{campaign.name}！首綱をプレゼント！")
+          Plugin.call(:minecraft_give_item, name, 'minecraft:lead', 1, 0, %<{display:{Name: "首綱",Lore:["自殺強化月間記念に#{name}がもらった", "#{today}"]}}>)
+        },
+        -> {
+          Plugin.call(:minecraft_tell, name, "#{campaign.name}！チャッカマンをプレゼント！")
+          Plugin.call(:minecraft_give_item, name, 'flint_and_steel', 1, 0, %<{display:{Name: "チャッカマン",Lore:["自殺強化月間記念に#{name}がもらった", "#{today}"]},ench:[{id:20,lvl:1}],AttributeModifiers:[{AttributeName:"generic.attackDamage",Name:"generic.attackDamage",Amount:4,Operation:0,UUIDMost:30253,UUIDLeast:21008}],HideFlags:3}>)
+        },
+        -> {
+          Plugin.call(:minecraft_tell, name, "#{campaign.name}！金床をプレゼント！")
+          Plugin.call(:minecraft_give_item, name, 'minecraft:anvil', 1, 0, %<{display:{Name: "金床",Lore:["自殺強化月間記念に#{name}がもらった", "#{today}"]},AttributeModifiers:[{AttributeName:"generic.knockbackResistance",Name:"generic.knockbackResistance",Amount:0.25,Operation:0,UUIDMost:65999,UUIDLeast:969951},{AttributeName:"generic.movementSpeed",Name:"generic.movementSpeed",Amount:-0.05,Operation:0,UUIDMost:11251,UUIDLeast:684314},{AttributeName:"generic.attackDamage",Name:"generic.attackDamage",Amount:5,Operation:0,UUIDMost:44111,UUIDLeast:12466}],ench:[{id:19,lvl:1}],HideFlags:3}>)
+        },
+      ].sample.()
+    },
   ]
 
   def self.active_campaigns
